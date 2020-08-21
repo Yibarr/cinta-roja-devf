0ba123e03eba7a11da010007ab6f5a9c8022f1d1
yibarr <yael@os.city>
2020-08-20 20:29:21 -0500

    1.3 Herencia

---
+++ b44/1.3 Herencia/main.js
@@ -0,0 +1,46 @@
+class Pet {
+  constructor(name, favoriteFood) {
+    this.name = name
+    this.favoriteFood = favoriteFood
+  }
+
+  eat(food = this.favoriteFood) {
+    return `Acá echando un emparedado de ${food}`
+  }
+
+}
+
+
+const Gunter = new Pet('Gunter', 'Sausages')
+
+
+
+
+
+
+class Dog extends Pet {
+  constructor(name, favoriteFood, color) {
+    super(name, favoriteFood)
+    this.color = color
+  }
+
+  ladrar() {
+    return `Toy ladrando`
+  }
+
+}
+
+
+const Milo = new Dog('Milo', 'Banana', 'black')
+
+console.log(Milo.eat())
+
+
+const feedPet = (pet, food = 'Acelgas') => {
+  console.log(pet.eat(food))
+}
+
+
+// feedPet(Gunter)
+
+console.log(Gunter, Milo);

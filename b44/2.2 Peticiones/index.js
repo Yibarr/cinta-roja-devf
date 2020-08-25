02891892ed774175ff01969b80b9d25c706af648
yibarr <yael@os.city>
2020-08-24 22:25:40 -0500

    NASA API

--- b44/2.2 Peticiones/index.js
+++ b44/2.2 Peticiones/index.js
@@ -38,8 +38,59 @@ const getPokemonMoves = (pokemonId) => {
   });
 };
 
+const NASA_URI = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-08-24&end_date=2020-08-30&api_key=7s0ep6LwhAf8zcd0EIQDjK1TptDPWvNUIRIXF1S8`
 
-getPokemonMoves(10)
+const getDangerousAsteroids = () => {
+  request.get(
+    `${NASA_URI}`,
+    (error, response) => {
+      if (response.statusCode === 200) {
+        const body =  JSON.parse(response.body)
+        
+        const NEOArr = Object.values(body.near_earth_objects)
+        // Objeto inicial, tomamos solo los valores del objeto y lo guardamos en la variable NEOArr
+        // const near_earth_objects = {
+        //   '2020-08-24': [],
+        //   '2020-08-25': []
+        // }
+
+
+        const flatDates = NEOArr.flat()
+        //[[{},{},{}], [{},{},{}], [{},{},{}]] => [{},{},{}, {},{},{}, {},{},{}] 
+
+
+        const dangerousAsteroids = flatDates.filter(asteroid => asteroid.is_potentially_hazardous_asteroid)
+        //arreglo filtrado
+        
+        console.log(dangerousAsteroids);
+        
+      } else {
+        console.log(`Hubo un error. Código: ${response.statusCode}`);
+        console.log(error, response);
+      }
+    }
+  )
+}
+
+
+// getDangerousAsteroids()
 
 //Crear función "getPokemonById" que reciba como parametro el nombre del pokemon y lo busque
 // Crear función "getPokemonTypes" e imprimirlos en la consola
+
+
+
+// const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+// console.log(arr.flat());
+
+
+
+
+
+//Despues de Object.values()
+
+
+
+
+// ['2020-08-24', '2020-08-25']

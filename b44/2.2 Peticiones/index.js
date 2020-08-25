7069ef2d59759c589f3c143de1353fa915b498b6
yibarr <yael@os.city>
2020-08-24 21:29:08 -0500

    pokemonByTypes b44

--- b44/2.2 Peticiones/index.js
+++ b44/2.2 Peticiones/index.js
@@ -14,9 +14,32 @@ const getDitto = () => {
   })
 }
 
+const getPokemonById = (pokemonId) => {
+  request.get(`${URI}/pokemon/${pokemonId}`, (error, response, body) => {
+    if (response.statusCode === 200) {
+      const parsedJson = JSON.parse(response.body);
+      console.log(`Información pokemon:`, parsedJson);
+    } else {
+      console.log(`Hubo un error de código: ${response.statusCode}`);
+    }
+  });
+};
 
-getDitto()
 
+const getPokemonMoves = (pokemonId) => {
+  request.get(`${URI}/pokemon/${pokemonId}`, (error, response, body) => {
+    if (response.statusCode === 200) {
+      const parsedJson = JSON.parse(response.body);
+      const movesNames = parsedJson.moves.map(move => move.move.name)
+      console.log(movesNames);
+    } else {
+      console.log(`Hubo un error de código: ${response.statusCode}`);
+    }
+  });
+};
+
+
+getPokemonMoves(10)
 
 //Crear función "getPokemonById" que reciba como parametro el nombre del pokemon y lo busque
 // Crear función "getPokemonTypes" e imprimirlos en la consola

b0e014af74fd40d5a68cf99c72a5733942f5a8c9
yibarr <yael@os.city>
2020-08-28 20:36:44 -0500

    react components

---
+++ b44/3.1 React/cinta-roja/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import './App.css';
+import Saludo from './components/Saludo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <p>Bienvenidos al himalaya</p>
+      <Saludo/>
+      <Saludo/>
+      <Saludo/>
+      <Saludo/>
+      <Saludo/>
+      <Saludo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ b44/3.1 React/cinta-roja/src/components/Saludo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Saludo = () => {
+  return (
+    <h1>este es un saludo a toda la banda</h1>
+  )
+}
+
+export default Saludo
